3f73e182dd0c2d98a12269f4b1672e9a7ef2d1bc
Makarova Elizaveta <elmak2308@gmail.com>
2025-06-10 10:47:09 +0500

    Нужно: Отправить запрос в консоль и получить ответ автоматически. Получать от пользователя данные (запрос, его роль и прочие [см. quest_for_gpt]) generate_contents вернет файл *.json в указанную папку (в файле quest_for_gpt) Указан ID серверного бота для запросов

--- server/quest_for_gpt.py
+++ server/quest_for_gpt.py
@@ -1,13 +1,20 @@
-role = "Учитель"
-ask = ""
-direct = ""
-file_name = ""
-title = ""
-level_of_creative = float()
+# Получение из запроса пользователя (или указать значения по цумолчанию)
+role = "Учитель" # Роль пользователя
+ask = "Создай тест по физике на 15 вопросов" # Запрос к ИИ
+direct = "ВАША ПАПКА" # Место сохранения файла
+file_name = "Ответ.json" # Название файла
+title = "Тест" # Заголовок
+level_of_creative = float(0.1) #Можно добавить ползунок
 
+#!/bin/bash (Вводится в консоль)
+"""
+export FOLDER_ID=xxxxxxxxxxxxxxxxxxx
+yc iam create-token > ~/.iam_token
+"""
 
-folder_id = 'ИД СЕРВЕРНОГО БОТА'
+folder_id = 'b1goanfad3n1392h6v76' # Не меняется (ID серверного бота, отправляющего запросы)
 try:
-    iam_token = open('/home/dan/.iam_token').read().strip() # папка другая
+    iam_token = server.get_iam_token.strip() # Сослаться на файл или запустить это здесь (запрос токена через консоль)
 except IOError as e:
+    print("DEBUG: YOU CAN'T GET IAM-TOKEN, PLEASE TRY AGAIN")
     raise Exception(f'IAM_TOKEN not found with {e}')
