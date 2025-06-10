967afdb08c63bf86db5ab4be3e6d753e46a80692
Makarova Elizaveta <elmak2308@gmail.com>
2025-06-10 11:01:55 +0500

    Попытка получить результат от запроса в кинсоли

--- server/quest_for_gpt.py
+++ server/quest_for_gpt.py
@@ -1,4 +1,4 @@
-# Получение из запроса пользователя (или указать значения по цумолчанию)
+# Получение из запроса пользователя (или указать значения по умолчанию)
 role = "Учитель" # Роль пользователя
 ask = "Создай тест по физике на 15 вопросов" # Запрос к ИИ
 direct = "ВАША ПАПКА" # Место сохранения файла
@@ -12,9 +12,18 @@ export FOLDER_ID=xxxxxxxxxxxxxxxxxxx
 yc iam create-token > ~/.iam_token
 """
 
-folder_id = 'b1goanfad3n1392h6v76' # Не меняется (ID серверного бота, отправляющего запросы)
+import subprocess
+
+#command = "yc iam create-token > ~/.iam_token"
+command = "export IAM_TOKEN=`yc iam create-token`"
 try:
-    iam_token = server.get_iam_token.strip() # Сослаться на файл или запустить это здесь (запрос токена через консоль)
+    result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+    iam_token = result
+except subprocess.CalledProcessError as e:
+    print(f"Ошибка: {e}")
+    print(e.stderr)
 except IOError as e:
     print("DEBUG: YOU CAN'T GET IAM-TOKEN, PLEASE TRY AGAIN")
     raise Exception(f'IAM_TOKEN not found with {e}')
+
+folder_id = 'b1goanfad3n1392h6v76' # Не меняется (ID серверного бота, отправляющего запросы)
